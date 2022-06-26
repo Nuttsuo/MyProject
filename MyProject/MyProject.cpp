70cdb40a2d366401966a781d7734fb0fd451d38a
Thanyapong Lathhkul <khunnuttsu@outlook.com>
2022-06-26 16:34:44 +0700

    2*

--- MyProject/MyProject.cpp
+++ MyProject/MyProject.cpp
@@ -18,19 +18,28 @@ int main()
     istringstream ss(input);
 
     //go word by word 
+    int charCount = ss.str().length();
+
+    if (charCount > 99) {
+        cout << "input invalid more than 50 characters";
+        return 0;
+    }
+
     while (ss >> temp)
     {
         vectorOfString.emplace_back(temp);
-    }   
-    
+    }
+
 
     sort(vectorOfString.begin(), vectorOfString.end(), [](const auto& lhs, const auto& rhs) {
-        const auto result = mismatch(lhs.cbegin(), lhs.cend(), rhs.cbegin(), rhs.cend(), [](const auto& lhs, const auto& rhs) {return tolower(lhs) == tolower(rhs); });
-
+        const auto result = mismatch(lhs.cbegin(), lhs.cend(), rhs.cbegin(), rhs.cend(), [](const auto& lhs, const auto& rhs)
+            {
+                return tolower(lhs) == tolower(rhs);
+            });
         return result.second != rhs.cend() && (result.first == lhs.cend() || tolower(*result.first) < tolower(*result.second));
         });
 
-    for (auto& i : vectorOfString) {
+    for (auto i : vectorOfString) {
         cout << i;
     }
     return 0;
